519d4b085992c53214bbe2fd8a3963808f58c130
AdiletBuytukov <abuytukov@gmail.com>
2024-05-27 22:06:19 +0600

    Реализован функционал перехода на страницу при клике на разделы: 1.Премьеры текущего месяца 2.Топ ожидаемые фильмы 3.Топ лучшие фильмы 4.Цифровые релизы текущего месяца

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const API_KEY = '8c8e1a50-6322-4135-8875-5d40a5420d86';
 const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_MOVIES&page=1";
 const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+const API_URL_PREMIERES = "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=MAY";
+const API_URL_CLOSES_RELEASES = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=CLOSES_RELEASES&page=1";
+const API_URL_TOP_MOVIES = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=1";
+const API_URL_MONTH_RELEASES = "https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2024&month=JANUARY&page=1";
 
 getMovies(API_URL_POPULAR, 'items');
 
@@ -17,19 +21,19 @@ async function getMovies(url, key) {
         const data = respData[key] || respData.films || [];
         showMovies(data);
     } catch (error) {
-    console.log(error);
-}
+        console.log(error);
+    }
 
-function showMovies(data) {
-    const moviesEl = document.querySelector(".movies");
+    function showMovies(data) {
+        const moviesEl = document.querySelector(".movies");
 
-    document.querySelector('.movies').innerHTML = '';
+        document.querySelector('.movies').innerHTML = '';
 
-    data.forEach((movie) => {
-        const movieEl = document.createElement("div");
-        movieEl.classList.add("movie");
-        movieEl.innerHTML =
-            `<div class="movie__cover-inner">
+        data.forEach((movie) => {
+            const movieEl = document.createElement("div");
+            movieEl.classList.add("movie");
+            movieEl.innerHTML =
+                `<div class="movie__cover-inner">
                     <img src = "${movie.posterUrlPreview}"
                         alt = "${movie.nameRu}"
                         class = "movie_cover" />
@@ -41,20 +45,45 @@ function showMovies(data) {
                     <div class="movie__category">${movie.genres.map(genre => ` ${genre.genre}`)}</div>
                     <div class="movie__avarage moviea__average--green">${movie.ratingKinopoisk}</div>
             </div>`;
-        moviesEl.appendChild(movieEl);
-    });
-}
-
-const form = document.querySelector("form");
-const search = document.querySelector('.header__search');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
-    if (search.value) {
-        getMovies(apiSearchUrl, 'films');
-        search.value = '';
+            moviesEl.appendChild(movieEl);
+        });
     }
 
-})}
+    const form = document.querySelector("form");
+    const search = document.querySelector('.header__search');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
+        if (search.value) {
+            getMovies(apiSearchUrl, 'films');
+            search.value = '';
+        }
+    })
+
+    const premieres = document.querySelector('.premieres');
+
+    premieres.addEventListener('click', () => {
+        getMovies(API_URL_PREMIERES, 'items');
+    })
+
+    const releases = document.querySelector('.releases');
+
+    releases.addEventListener('click', () => {
+        getMovies(API_URL_CLOSES_RELEASES, 'items')
+    })
+
+    const popular_movie = document.querySelector('.popular_movie');
+
+    popular_movie.addEventListener('click', () => {
+        getMovies(API_URL_TOP_MOVIES, 'items')
+    })
+
+    const releases_month = document.querySelector('.releases_month');
+
+    releases_month.addEventListener('click', () => {
+        getMovies(API_URL_MONTH_RELEASES, 'releases')
+    })
+
+}
